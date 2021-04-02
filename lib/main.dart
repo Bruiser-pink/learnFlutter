ab77f1afebc28d924ef8cf3cecf1b07991b9de38
Bruiser-pink <1609526684@qq.com>
2021-04-02 15:33:35 +0800

    feat: 自定义widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 
-// //自定义widget
-// class MyAPP extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//
-//     )
-//   }
-// }
+//自定义widget
+class MyAPP extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: HomeContent()
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Checkbox(
+              value: true,
+              onChanged: (value) => print("Hello World")),
+          Text(
+            "同意协议",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
+}
 main(List<String> args) {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Checkbox(
-                  value: true,
-                  onChanged: (value) => print("Hello World")),
-              Text(
-                "同意协议",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 20),
-              )
-            ],
-          ),
-      ),
-    ),
-  ));
+  runApp(MyAPP());
 }
