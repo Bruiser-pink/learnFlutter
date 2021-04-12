4e064e852e7f4cd3e00d0c8966e5906fae72570b
Bruiser-pink <1609526684@qq.com>
2021-04-12 14:34:58 +0800

    feat: StatefulWidget 计数器

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ProductItem extends StatelessWidget{
-  //无状态组件的变量都应该用final类型
-  final String title;
-  final String desc;
-  final String imgUrl;
-  ProductItem(this.title, this.desc, this.imgUrl);
+class MycountWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        border: Border.all()
-      ),
-      child:  Column(
-        children: <Widget>[
-          Text(title, style: TextStyle(fontSize: 24),),
-          Text(desc, style: TextStyle(fontSize: 18),),
-          SizedBox(height: 10,),
-          Image.network(imgUrl)
-        ],
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return MycountState();
   }
 }
 
-class HomeContent extends StatelessWidget {
+class MycountState extends State<MycountWidget> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: <Widget>[
-          ProductItem("Apple1", "Macbook Product1", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
-          ProductItem("Apple2", "Macbook Product2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
-          ProductItem("Apple3", "Macbook Product3", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg"),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+       children: <Widget>[
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             RaisedButton(
+               color: Colors.redAccent,
+               child: Text("+1", style: TextStyle(fontSize: 18, color: Colors.white),),
+               onPressed: () {
+                 setState(() {
+                   count++;
+                 });
+               },
+             ),
+             RaisedButton(
+               color: Colors.orangeAccent,
+               child: Text("-1", style: TextStyle(fontSize: 18, color: Colors.white),),
+               onPressed: () {
+                 setState(() {
+                   count--;
+                 });
+               },
+             )
+           ],
+         ),
+         Text('当前计数$count', style: TextStyle(fontSize: 30),),
+       ]
     );
   }
 }
@@ -50,7 +52,7 @@ class MyAPP extends StatelessWidget{
           appBar: AppBar(
             title: Text('Home'),
           ),
-          body: HomeContent()
+          body: MycountWidget()
       ),
     );
   }
