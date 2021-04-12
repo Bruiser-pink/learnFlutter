3454519bc367aefe19c5d25fd82fb7d002ed367b
Bruiser-pink <1609526684@qq.com>
2021-04-12 19:06:16 +0800

    feat: dio 网络请求

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 main(List<String> args) {
   runApp(MyAPP());
 }
 
 class MyAPP extends StatelessWidget {
+  static getInfo() async {
+    try {
+      var result = await new Dio().get('https://easytrave.xyz');
+      result = null;
+      if(result == null) {
+        throw 'get info Error';
+      }
+      print(result);
+    } catch(e) {
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,69 +27,23 @@ class MyAPP extends StatelessWidget {
         appBar: AppBar(
           title: Text('我是标题'),
         ),
-        body: ContentWidget(),
+        body: Column(
+          children: [
+            Text(
+              'textAlign：文本对齐方式，比如TextAlign.centermaxLines\n：最大显示行数，比如1overflow：超出部分显示方式，比如TextOverflow.ellipsistextScaleFactor：\n控制文本缩放，比如1.24',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            TextButton(onPressed: () => print('FlatButton'), child: Text('textButton')),
+            TextButton(onPressed: () => getInfo(), child: Text('fetch network request', style: TextStyle(fontSize: 20),))
+          ],
+        )
       ),
     );
   }
 }
 
-class ContentWidget extends StatefulWidget {
-  //生命周期函数尝
-  ContentWidget() {
-    print('1: StatefulWidget constructors is running');
-  }
-  @override
-  State<StatefulWidget> createState() {
-    print("2: createState is running");
-    return ContentStateWidget();
-  }
-}
-
-class ContentStateWidget extends State<ContentWidget> {
-  int counter = 0;
-  ContentStateWidget() {
-    print("3: State Constructors is running");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print("4: initState is running");
-  }
-
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-    print("didChangeDependencies is running");
-  }
-
-  @override
-  void didUpdateWidget(covariant ContentWidget oldWidget) {
-    // TODO: implement didUpdateWidget
-    super.didUpdateWidget(oldWidget);
-    print("didUpdateWidget is running");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("5: State build is running");
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("hello world $counter", style: TextStyle(fontSize: 30),),
-          RaisedButton(
-            color: Colors.orangeAccent,
-            child: Text("counter+1", style: TextStyle(fontSize: 18, color: Colors.white),),
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            },
-          )
-        ],
-      )
-    );
-  }
-}
