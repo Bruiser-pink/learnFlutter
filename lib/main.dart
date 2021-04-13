98b31a5eca51f9762a1fc8886dff32b61708773e
Bruiser-pink <1609526684@qq.com>
2021-04-13 11:39:09 +0800

    feat: mySelf counter demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,56 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 
-main(List<String> args) {
-  runApp(MyAPP());
-}
+void main() => runApp(MyApp());
 
-class MyAPP extends StatelessWidget {
-  static getInfo() async {
-    try {
-      var result = await new Dio().get('https://easytrave.xyz');
-      result = null;
-      if(result == null) {
-        throw 'get info Error';
-      }
-      print(result);
-    } catch(e) {
-      print(e);
-    }
-  }
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello world',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('我是标题'),
-        ),
-        body: Column(
-          children: [
-            Text(
-              'textAlign：文本对齐方式，比如TextAlign.centermaxLines\n：最大显示行数，比如1overflow：超出部分显示方式，比如TextOverflow.ellipsistextScaleFactor：\n控制文本缩放，比如1.24',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-            TextButton(onPressed: () => print('FlatButton'), child: Text('textButton')),
-            TextButton(onPressed: () => getInfo(), child: Text('fetch network request', style: TextStyle(fontSize: 20),))
-          ],
-        )
+    return new MaterialApp(
+      title: "Flutter App",
+      theme: ThemeData(
+        primaryColor: Colors.blue
       ),
+      home: MyHomePage()
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyStateHomePage();
+  }
+}
+
+class MyStateHomePage extends State<MyHomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page', style: TextStyle(fontSize: 20),),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('current times', style: TextStyle(fontSize: 30, color: Colors.black),),
+            Text("$counter", style: TextStyle(fontSize: 40, color: Colors.black12), textAlign: TextAlign.center,)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()  {
+          setState(() {
+            counter++;
+          });
+        },
+        tooltip: "add counter",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
