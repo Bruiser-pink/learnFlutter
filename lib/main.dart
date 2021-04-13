708e7807c7ec7227e308890ad091da31ccaf3108
Bruiser-pink <1609526684@qq.com>
2021-04-13 17:28:06 +0800

    feat: try route

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: "Flutter App",
       theme: ThemeData(
         primaryColor: Colors.blue
       ),
-      home: MyHomePage()
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => MyHomePage(),
+        '/about': (BuildContext context) => MyAboutPage()
+      },
+      initialRoute: '/'
     );
   }
 }
@@ -27,6 +31,17 @@ class MyHomePage extends StatefulWidget {
 
 class MyStateHomePage extends State<MyHomePage> {
   int counter = 0;
+  int currentIndex = 0;
+  _handleBarTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+    if(currentIndex == 0) {
+      Navigator.pushNamed(context, '/');
+    }else {
+      Navigator.pushNamed(context, '/about');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +57,22 @@ class MyStateHomePage extends State<MyHomePage> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'about'
+          ),
+        ],
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _handleBarTap
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: ()  {
           setState(() {
@@ -50,6 +81,17 @@ class MyStateHomePage extends State<MyHomePage> {
         },
         tooltip: "add counter",
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyAboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("this is about page", style: TextStyle(fontSize: 30),),
       ),
     );
   }
